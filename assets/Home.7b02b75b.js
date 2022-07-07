a4da2e6e6e4082228a06edf4e0d2bbd8180fe9d4
ZxBing0066 <ZxBing0066@gmail.com>
2022-07-07 04:08:09 +0000

    Deploying to gh-pages from @ ZxBing0066/zreact@4560b986910aeaad0a13ef0290bb532329de1b03 🚀

---
+++ assets/Home.7b02b75b.js
@@ -0,0 +1,250 @@
+import{R as e}from"./index.97b01244.js";import{u as m,w as x,N as o,a as f,C as u}from"./app.fb33141b.js";const w=`
+.home-hero {
+  margin: 2.5rem 0 2.75rem;
+  padding: 0 1.5rem;
+  text-align: center;
+}
+
+@media (min-width: 420px) {
+  .home-hero {
+    margin: 3.5rem 0;
+  }
+}
+
+@media (min-width: 720px) {
+  .home-hero {
+    margin: 4rem 0 4.25rem;
+  }
+}
+
+.figure {
+  padding: 0 1.5rem;
+}
+
+.image {
+  display: block;
+  margin: 0 auto;
+  width: auto;
+  max-width: 100%;
+  max-height: 280px;
+}
+
+.title {
+  margin-top: 1.5rem;
+  font-size: 2rem;
+}
+
+@media (min-width: 420px) {
+  .title {
+    font-size: 3rem;
+  }
+}
+
+@media (min-width: 720px) {
+  .title {
+    margin-top: 2rem;
+  }
+}
+
+.tagline {
+  margin: 0;
+  margin-top: 0.25rem;
+  line-height: 1.3;
+  font-size: 1.2rem;
+  color: var(--c-text-light);
+}
+
+@media (min-width: 420px) {
+  .tagline {
+    line-height: 1.2;
+    font-size: 1.6rem;
+  }
+}
+
+.action {
+  margin-top: 1.5rem;
+  display: inline-block;
+}
+
+.action.alt {
+  margin-left: 1.5rem;
+}
+
+@media (min-width: 420px) {
+  .action {
+    margin-top: 2rem;
+    display: inline-block;
+  }
+}
+
+.action :deep(.item) {
+  display: inline-block;
+  border-radius: 6px;
+  padding: 0 20px;
+  line-height: 44px;
+  font-size: 1rem;
+  font-weight: 500;
+  color: var(--c-bg);
+  background-color: var(--c-brand);
+  border: 2px solid var(--c-brand);
+  transition: background-color 0.1s ease;
+}
+
+.action.alt :deep(.item) {
+  background-color: var(--c-bg);
+  color: var(--c-brand);
+}
+
+.action :deep(.item:hover) {
+  text-decoration: none;
+  color: var(--c-bg);
+  background-color: var(--c-brand-light);
+}
+
+@media (min-width: 420px) {
+  .action :deep(.item) {
+    padding: 0 24px;
+    line-height: 52px;
+    font-size: 1.2rem;
+    font-weight: 500;
+  }
+}
+`;var E=()=>{const{site:a,frontmatter:t}=m(),n=(()=>{const{heroImage:s,heroText:d,tagline:h,actionLink:p,actionText:g}=t;return s||d||h||p&&g})(),i=()=>t.heroText||a.title,r=()=>t.tagline||a.description,l={link:t.altActionLink,text:t.altActionText},c={link:t.actionLink,text:t.actionText};return e.createElement(e.Fragment,null,e.createElement("style",null,w),n&&e.createElement("header",{className:"home-hero"},t.heroImage&&e.createElement("figure",{className:"figure"},e.createElement("img",{className:"image",src:x(t.heroImage),alt:t.heroAlt})),i&&e.createElement("h1",{id:"main-title",className:"title"},i),r&&e.createElement("p",{className:"tagline"},r),t.actionLink&&t.actionText&&e.createElement(o,{item:c,className:"action"}),t.altActionLink&&t.altActionText&&e.createElement(o,{item:l,className:"action alt"})))};const v=`
+.home-features {
+  margin: 0 auto;
+  padding: 2.5rem 0 2.75rem;
+  max-width: 960px;
+}
+
+.home-hero + .home-features {
+  padding-top: 0;
+}
+
+@media (min-width: 420px) {
+  .home-features {
+    padding: 3.25rem 0 3.5rem;
+  }
+
+  .home-hero + .home-features {
+    padding-top: 0;
+  }
+}
+
+@media (min-width: 720px) {
+  .home-features {
+    padding-right: 1.5rem;
+    padding-left: 1.5rem;
+  }
+}
+
+.wrapper {
+  padding: 0 1.5rem;
+}
+
+.home-hero + .home-features .wrapper {
+  border-top: 1px solid var(--c-divider);
+  padding-top: 2.5rem;
+}
+
+@media (min-width: 420px) {
+  .home-hero + .home-features .wrapper {
+    padding-top: 3.25rem;
+  }
+}
+
+@media (min-width: 720px) {
+  .wrapper {
+    padding-right: 0;
+    padding-left: 0;
+  }
+}
+
+.container {
+  margin: 0 auto;
+  max-width: 392px;
+}
+
+@media (min-width: 720px) {
+  .container {
+    max-width: 960px;
+  }
+}
+
+.features {
+  display: flex;
+  flex-wrap: wrap;
+  margin: -20px -24px;
+}
+
+.feature {
+  flex-shrink: 0;
+  padding: 20px 24px;
+  width: 100%;
+}
+
+@media (min-width: 720px) {
+  .feature {
+    width: calc(100% / 3);
+  }
+}
+
+.title {
+  margin: 0;
+  border-bottom: 0;
+  line-height: 1.4;
+  font-size: 1.25rem;
+  font-weight: 500;
+}
+
+@media (min-width: 420px) {
+  .title {
+    font-size: 1.4rem;
+  }
+}
+
+.details {
+  margin: 0;
+  line-height: 1.6;
+  font-size: 1rem;
+  color: var(--c-text-light);
+}
+
+.title + .details {
+  padding-top: 0.25rem;
+}
+`;var b=()=>{const{frontmatter:a}=m(),t=a.features&&a.features.length>0,n=a.features?a.features:[];return e.createElement(e.Fragment,null,e.createElement("style",null,v),t&&e.createElement("div",{className:"home-features"},e.createElement("div",{className:"wrapper"},e.createElement("div",{className:"container"},e.createElement("div",{className:"features"},n.map((i,r)=>e.createElement("section",{key:r,className:"feature"},i.title&&e.createElement("h2",{className:"title"},i.title),i.details&&e.createElement("p",{className:"details"},i.details))))))))};const N=`
+.footer {
+  margin: 0 auto;
+  max-width: 960px;
+}
+
+@media (min-width: 720px) {
+  .footer {
+    padding: 0 1.5rem;
+  }
+}
+
+.container {
+  padding: 2rem 1.5rem 2.25rem;
+}
+
+.home-hero + .footer .container,
+.home-features + .footer .container,
+.home-content + .footer .container {
+  border-top: 1px solid var(--c-divider);
+}
+
+@media (min-width: 420px) {
+  .container {
+    padding: 3rem 1.5rem 3.25rem;
+  }
+}
+
+.text {
+  margin: 0;
+  text-align: center;
+  line-height: 1.4;
+  font-size: 0.9rem;
+  color: var(--c-text-light);
+}
+`;var k=()=>{const{frontmatter:a}=m();return e.createElement(e.Fragment,null,e.createElement("style",null,N),a.footer&&e.createElement("footer",{className:"footer"},e.createElement("div",{className:"container"},e.createElement("p",{className:"text"},a.footer))))};const y="_home_1ognm_1";var z={home:y};const A=()=>e.createElement("main",{className:f(z,"home"),"aria-labelledby":"main-title"},e.createElement(E,null),e.createElement("slot",{name:"hero"}),e.createElement(b,null),e.createElement("div",{className:"home-content"},e.createElement(u,null)),e.createElement("slot",{name:"features"}),e.createElement(k,null),e.createElement("slot",{name:"footer"}));export{A as default};
---
+++ assets/useOverflow.md.b43febb9.lean.js
@@ -0,0 +1,52 @@
+import"./index.97b01244.js";const a='{"title":"useOverflow","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u4F7F\u7528\u573A\u666F","slug":"\u4F7F\u7528\u573A\u666F"},{"level":2,"title":"\u5B89\u88C5","slug":"\u5B89\u88C5"},{"level":2,"title":"\u4F7F\u7528","slug":"\u4F7F\u7528"},{"level":2,"title":"API","slug":"api"},{"level":2,"title":"DEMO","slug":"demo"}],"relativePath":"useOverflow.md","createTime":1657166831000,"lastUpdated":1657166831000}';var n=()=>`<h1 id="useoverflow" tabindex="-1">useOverflow <a class="header-anchor" href="#useoverflow" aria-hidden="true">#</a></h1>
+<h2 id="\u4F7F\u7528\u573A\u666F" tabindex="-1">\u4F7F\u7528\u573A\u666F <a class="header-anchor" href="#\u4F7F\u7528\u573A\u666F" aria-hidden="true">#</a></h2>
+<p>\u5F53\u4F7F\u7528\u6A2A\u5411\u63A7\u4EF6\u7EC4\uFF0C\u5BB9\u5668\u5BBD\u5EA6\u672A\u77E5\uFF0C\u53C8\u4E0D\u5E0C\u671B\u63A7\u4EF6\u8D85\u51FA\u5BB9\u5668\u8303\u56F4\u6216\u6362\u884C\u65F6\uFF0C\u53EF\u4EE5\u4F7F\u7528 <code>useOverflow</code> \u6765\u8BA9\u63A7\u4EF6\u81EA\u52A8\u8C03\u6574\u5BBD\u5EA6\u3002</p>
+<p>\u5E38\u89C1\u4F7F\u7528\u573A\u666F\u5982\uFF1A</p>
+<ul>
+<li>\u6807\u7B7E\u7EC4\u81EA\u52A8\u7F29\u7565</li>
+<li>\u6309\u94AE\u7EC4\u5BBD\u5EA6\u4E0D\u591F\u7F29\u7565\u4E3A\u66F4\u591A</li>
+</ul>
+<p><img src="images/button-demo.png" alt="">
+<img src="images/tag-demo.png" alt=""></p>
+<h2 id="\u5B89\u88C5" tabindex="-1">\u5B89\u88C5 <a class="header-anchor" href="#\u5B89\u88C5" aria-hidden="true">#</a></h2>
+<div class="language-sh"><pre><code>npm install @zreact/use-overflow
+</code></pre>
+</div><h2 id="\u4F7F\u7528" tabindex="-1">\u4F7F\u7528 <a class="header-anchor" href="#\u4F7F\u7528" aria-hidden="true">#</a></h2>
+<div class="language-jsx"><pre><code><span class="token comment">// count \u4E3A\u5F53\u524D\u4F7F\u7528\u7684\u6D4B\u91CF\u6570\u91CF</span>
+<span class="token comment">// measuredCount \u4E3A\u7ECF\u8FC7\u6D4B\u91CF\u540E\u6700\u7EC8\u5F97\u5230\u7684\u5408\u9002\u7684\u6570</span>
+<span class="token keyword">const</span> <span class="token punctuation">[</span>count<span class="token punctuation">,</span> measuredCount<span class="token punctuation">]</span> <span class="token operator">=</span> <span class="token function">useOverflow</span><span class="token punctuation">(</span><span class="token punctuation">{</span>
+    <span class="token literal-property property">containerRef</span><span class="token operator">:</span> containerRef<span class="token punctuation">,</span> <span class="token comment">// \u5916\u5C42\u5BB9\u5668\u7684 ref</span>
+    <span class="token literal-property property">total</span><span class="token operator">:</span> tags<span class="token punctuation">.</span>length <span class="token comment">// \u9879\u76EE\u7684\u603B\u6570</span>
+<span class="token punctuation">}</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+</code></pre>
+</div><h2 id="api" tabindex="-1">API <a class="header-anchor" href="#api" aria-hidden="true">#</a></h2>
+<h2 id="demo" tabindex="-1">DEMO <a class="header-anchor" href="#demo" aria-hidden="true">#</a></h2>
+<p>\u7B80\u5355\u573A\u666F</p>
+<iframe src="https://codesandbox.io/embed/use-overflow-demo-3wf8c4?autoresize=1&fontsize=14&hidenavigation=1&theme=dark"
+    style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+    title="use-overflow demo"
+    allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+    sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+></iframe>
+<p>\u5185\u5BB9\u53EF\u53D8\u573A\u666F</p>
+<iframe src="https://codesandbox.io/embed/use-overflow-demo-editable-4eb7ky?autoresize=1&fontsize=14&hidenavigation=1&theme=dark"
+    style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+    title="use-overflow demo editable"
+    allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+    sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+></iframe>
+<p>\u5185\u5BB9\u53EF\u7F16\u8F91\u573A\u666F</p>
+<iframe src="https://codesandbox.io/embed/use-overflow-demo-inputable-pzsde4?autoresize=1&fontsize=14&hidenavigation=1&theme=dark"
+    style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+    title="use-overflow demo inputable"
+    allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+    sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+></iframe>
+<p>\u6309\u94AE\u7EC4</p>
+<iframe src="https://codesandbox.io/embed/use-overflow-button-demo-0eby5y?autoresize=1&fontsize=14&hidenavigation=1&theme=dark"
+    style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+    title="use-overflow button demo"
+    allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+    sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+></iframe>
+`;export{a as __pageData,n as default};
